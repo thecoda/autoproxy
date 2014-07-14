95f61b195c2864702ea8a34d53d3c611410d3191
Kevin Wright <kev.lee.wright@gmail.com>
2014-07-14 23:15:54 +0100

    Cleaning up dependencies and suchlike

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -21,22 +21,16 @@ object Dependencies {
     //lazy val scalaz     = "org.scalaz"                 %% "scalaz-core"                   % "7.1.0-M4"
     //lazy val shapeless   = "com.chuusai"                %% "shapeless"                     % "2.0.0-SNAPSHOT"
     lazy val paradise      = "org.scalamacros"            %  "paradise_2.11.1"               % "2.1.0-M1"
-    lazy val slf4j         = "org.slf4j"                  %  "slf4j-api"                     % "1.7.5"
+    lazy val slf4j         = "org.slf4j"                  %  "slf4j-api"                     % "1.7.6"
     lazy val all = Seq(paradise, slf4j) //shapeless, scalaz
   }
 
   object Test {
-    lazy val logback    = "ch.qos.logback"             %  "logback-classic"               % "1.1.0"   % "test"
-    lazy val groovy     = "org.codehaus.groovy"        %  "groovy-all"                    % "1.7.6"   % "test"
-    lazy val janino     = "org.codehaus.janino"        %  "janino"                        % "2.6.1"  % "test"
-//    lazy val specs2     = "org.specs2"                 %% "specs2"                        % "2.0-RC1" % "test"
+    lazy val logback    = "ch.qos.logback"             %  "logback-classic"               % "1.1.2"   % "test"
     lazy val scalatest  = "org.scalatest"              %% "scalatest"                     % "2.2.0"   % "test"
-    lazy val scalacheck = "org.scalacheck"             %% "scalacheck"                    % "1.11.4"  % "test"
-    lazy val mockito    = "org.mockito"                %  "mockito-core"                  % "1.9.0"   % "test"
-    lazy val hamcrest   = "org.hamcrest"               %  "hamcrest-core"                 % "1.3"     % "test"
     lazy val junit      = "junit"                      %  "junit"                         % "4.7"     % "test" //for xml output
 
-    lazy val all = Seq(logback, groovy, janino, scalatest, scalacheck, mockito, hamcrest)
+    lazy val all = Seq(logback, scalatest, junit)
 
   }
 
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -6,14 +6,12 @@ resolvers ++= Seq(
   "scct-github-repository" at "http://mtkopone.github.com/scct/maven-repo"
 )
 
-addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.4.0")
-
 addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.6.0")
 
 addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.7.4")
 
-addSbtPlugin("com.github.gseitz" % "sbt-release" % "0.8")
+addSbtPlugin("com.github.gseitz" % "sbt-release" % "0.8.3")
 
-addSbtPlugin("no.arktekk.sbt" % "aether-deploy" % "0.10")
+addSbtPlugin("no.arktekk.sbt" % "aether-deploy" % "0.11")
 
 
